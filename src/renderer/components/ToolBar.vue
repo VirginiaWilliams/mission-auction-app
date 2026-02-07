d82a17a707048329072f90107ff4a0bdd3884a5c
VirginiaWilliams <ginnyglink@gmail.com>
2026-02-07 13:47:09 -0800

    added bidder csv button

--- src/renderer/components/ToolBar.vue
+++ src/renderer/components/ToolBar.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="toolbar">
-    <label for="file-input" class="button add-csv"> Upload Package CSV </label>
-    <input id="file-input" type="file" hidden @change="handleFileSelect" />
+    <div class="package-button">
+      <label for="file-input" class="button add-package">
+        Upload Package CSV
+      </label>
+      <input id="file-input" type="file" hidden @change="handlePackageUpload" />
+    </div>
+    <div class="package-button">
+      <label for="file-input" class="button add-bidder">
+        Upload Bidder CSV
+      </label>
+      <input id="file-input" type="file" hidden @change="handleBidderUpload" />
+    </div>
   </div>
 </template>
 
@@ -11,7 +21,30 @@ import { useStore } from "vuex";
 
 const store = useStore();
 
-function handleFileSelect(e) {
+function handlePackageUpload(e) {
+  const file = e.target.files[0];
+
+  if (file) {
+    Papa.parse(file, {
+      delimiter: ",",
+      header: true,
+      comments: false,
+      complete: function (results) {
+        for (let i = 0; i < results.data.length - 1; i++) {
+          let data = {};
+
+          data.type = results.data[i].type;
+          data.name = results.data[i].description;
+          data.value = results.data[i].value;
+
+          store.dispatch("createAucItem", data);
+        }
+      },
+    });
+  }
+}
+
+function handleBidderUpload(e) {
   const file = e.target.files[0];
 
   if (file) {
@@ -22,13 +55,13 @@ function handleFileSelect(e) {
       complete: function (results) {
         for (let i = 0; i < results.data.length - 1; i++) {
           console.log("results: ", results.data[i]);
-          let data = {};
+          // let data = {};
 
-          data.type = results.data[i].type;
-          data.name = results.data[i].description;
-          data.value = results.data[i].value;
+          // data.type = results.data[i].type;
+          // data.name = results.data[i].description;
+          // data.value = results.data[i].value;
 
-          store.dispatch("createAucItem", data);
+          // store.dispatch("createAucItem", data);
         }
       },
     });
@@ -46,8 +79,14 @@ function handleFileSelect(e) {
   align-items: center;
 }
 
-.add-csv {
+.add-package {
   background-color: cornflowerblue;
+  margin-left: 1rem;
+  height: 1rem;
+}
+
+.add-bidder {
+  background-color: slateblue;
   margin-left: 1rem;
   height: 1rem;
 }
